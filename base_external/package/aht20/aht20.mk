17d5f0e885b2a0fbf2a03f75a407c4fbab2b8019
Madeleine Monfort <mamo6538@colorado.edu>
2024-04-20 22:24:14 -0700

    buildroot used for i2c driver included

--- base_external/package/aht20/aht20.mk
+++ base_external/package/aht20/aht20.mk
@@ -5,17 +5,17 @@
 #
 ##############################################################
 
-AESDCHAR_VERSION = c92a6c9c02d5001c498d9937329d0daaaf9c9c05
-AESDCHAR_SITE = git@github.com:cu-ecen-aeld/final-project-mamo6538.git
-AESDCHAR_SITE_METHOD = git
-AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+AHT20_VERSION = 98e03e91105ad77f8e9d0a5b149fffd92f04ee54
+AHT20_SITE = git@github.com:cu-ecen-aeld/final-project-mamo6538.git
+AHT20_SITE_METHOD = git
+AHT20_GIT_SUBMODULES = NO
 
-AESDCHAR_MODULE_SUBDIRS = aht20_driver/
-AESDCHAR_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
+AHT20_MODULE_SUBDIRS = aht20_driver/
+AHT20_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
 
 $(eval $(kernel-module))
 
-define AESDCHAR_INSTALL_TARGET_CMDS
+define AHT20_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/aht20_driver/aht20_load $(TARGET_DIR)/usr/bin/aht20_load
 	$(INSTALL) -m 0755 $(@D)/aht20_driver/aht20_unload $(TARGET_DIR)/usr/bin/aht20_unload
 	
